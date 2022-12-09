882e1167e0abcafc00d1a2162eabe11542db7732
Alain <jeana@globalhitss.com>
2022-12-09 00:57:15 -0300

    .env.example

--- src/database/connection.js
+++ src/database/connection.js
@@ -2,16 +2,17 @@ const mongoose = require("mongoose");
 // Configuration to access the environment variables
 require("dotenv").config();
 
-const MONGO_DB_USER = process.env.MONGO_DB_USER;
-const MONGO_DB_SECRET = process.env.MONGO_DB_SECRET;
-const MONGO_DB_NAME = process.env.MONGO_DB_NAME;
-const MONGO_DB_URI = `mongodb+srv://${MONGO_DB_USER}:${MONGO_DB_SECRET}@cluster0.ascilwc.mongodb.net/${MONGO_DB_NAME}?retryWrites=true&w=majority`;
+const MONGODB_USER = process.env.MONGODB_USER;
+const MONGODB_SECRET = process.env.MONGODB_SECRET;
+const MONGODB_NAME = process.env.MONGODB_NAME;
+const MONGODB_CLUSTER = process.env.MONGODB_CLUSTER
+const MONGODB_URI = `mongodb+srv://${MONGODB_USER}:${MONGODB_SECRET}@${MONGODB_CLUSTER}/${MONGODB_NAME}?retryWrites=true&w=majority`;
 
 const dbConnect = async () => {
-  console.log(MONGO_DB_URI);
+  console.log(MONGODB_URI);
   try {
-    console.log(MONGO_DB_URI);
-    await mongoose.connect(MONGO_DB_URI, {
+    console.log(MONGODB_URI);
+    await mongoose.connect(MONGODB_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
